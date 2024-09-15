db1173bf8ffb9cee6d7bbb24aa7cb81d7f67241f
AhmadAfsheh <a.afsha2912+1@gmail.com>
2024-09-15 23:33:02 +0300

    add the relations to the Comment and Post Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'user_id'];
+    
+    // A post belongs to a user (admin)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // A post has many comments
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
+
